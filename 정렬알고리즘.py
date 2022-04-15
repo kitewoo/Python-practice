846cbe5875a98bbbff577393308c647a19324317
kitewoo <dusdn1110@naver.com>
2022-04-15 20:41:58 +0900

    20220415

--- 정렬알고리즘.py
+++ 정렬알고리즘.py
@@ -20,5 +20,57 @@ for i in range(len(array)):
             array[j], array[j-1] = array[j-1], array[j]
         else:
             break
+#삽입 정렬은 이중for문이라 위와 동일하게 최대 O(N**2)이다. 만약 정렬이 잘 되어있다면 N번만 하면 되므로 최선의 경우 O(N)이다. 
 
 print(array)
+
+# 퀵 정렬 : 기준 데이터 값을 기준으로 큰 값과 작은 값의 위치를 바꾸는 방법. 정렬 라이브러리의 근간이 되는 알고리즘
+# 시간 복잡도는 최선의 경우 O(N*log(n)), 최악의 경우 N**2
+array = [5,7,9,0,3,1,6,2,4,8]
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right=end
+    while(left <= right): 
+        #pivot 보다 큰 데이터를 찾을 때 까지 반복
+        while(left <= end and array[left] <= array[pivot]):
+            left += 1
+        #pivot 보다 작은 데이터를 찾을 때 까지 반복
+        while (right > start and array[right] >= array[pivot]):
+            right -= 1 
+        
+        if(left>right): #왼쪽에서 오는 애랑 오른쪽에서 오는 애랑 엇갈렸다면
+            #작은 데이터와 피벗을 교체
+            array[right], array[pivot] = array[pivot], array[right]
+
+        else:
+            array[left], array[right] = array[right], array[left]
+        
+    #분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행(재귀)
+
+    quick_sort(array, start, right-1)
+    quick_sort(array,right+1, end)
+quick_sort(array,0,len(array)-1)
+print(array)
+
+
+#파이썬 장점을 살린 방식
+array= [5,7,9,0,3,1,6,2,4,8]
+
+def quick_sort(array):
+    #리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <=1:
+        return array
+    
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x<=pivot] # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] #분할된 오른쪽 부분
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
+
